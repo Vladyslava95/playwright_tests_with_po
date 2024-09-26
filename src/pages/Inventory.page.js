c86a0143ee45b35194079d711190988b15a91426
Vlada Terkhova <vladaterkhova@gmail.com>
2024-09-26 21:36:53 +0300

    SortTest

--- src/pages/Inventory.page.js
+++ src/pages/Inventory.page.js
@@ -1,15 +1,24 @@
 import { BaseSwagLabPage } from './BaseSwagLab.page';
 
 export class InventoryPage extends BaseSwagLabPage {
+
     url = '/inventory.html';
 
     headerTitle = this.page.locator('.title');
 
     inventoryItems = this.page.locator('.inventory_item');
 
+    get inventoryItemName() { return this.page.locator('.inventory_item_name'); }
+
+    get inventoryPrice() { return this.page.locator('.inventory_item_price'); }   
+
     addItemToCartButton = this.page.locator('[id^="add-to-cart"]');
 
-    async addItemToCartById(id) {
+    sortDropdown = this.page.locator('.product_sort_container');     
+   
+
+   async addItemToCartById(id) {
         await this.addItemToCartButton.nth(id).click();
-    }
+   }       
+    
 }
--- src/tests/example.spec.js
+++ src/tests/example.spec.js
@@ -28,3 +28,83 @@ test.describe('Saucedemo app basic tests', () => {
         await expect(app.shoppingCart.cartItems).not.toBeAttached();
     });
 });
+
+test.describe('Saucedemo Unit 10 tests', () => {
+    test('sorting z-a', async (
+        /** @type {{ app: import('../pages/Application').Application }} */{ app },
+    ) => {
+        await app.login.navigate();
+        await app.login.performLogin('standard_user', 'secret_sauce');       
+        
+        const zaOption =  {sort: "za", getExpectedResult: (inventoryInitialOrder) => {
+            return inventoryInitialOrder.sort((a, b) => b.localeCompare(a));
+            }}        
+        const inventoryInitialOrder = await app.inventory.inventoryItemName.allTextContents();
+        await app.inventory.sortDropdown.selectOption({ value: zaOption.sort});
+        const actualOrder = await app.inventory.inventoryItemName.allTextContents();
+        const expectedOrder = zaOption.getExpectedResult(inventoryInitialOrder)
+        expect(actualOrder).toEqual(expectedOrder);
+
+        const azOption = {sort: "az", getExpectedResult: (inventoryInitialOrder) => {
+            return productInitialOrder.sort();
+            }}        
+
+    });
+    test('sorting a-z', async (
+        /** @type {{ app: import('../pages/Application').Application }} */{ app },
+    ) => {
+        await app.login.navigate();
+        await app.login.performLogin('standard_user', 'secret_sauce');
+        const azOption = {sort: "az", getExpectedResult: (inventoryInitialOrder) => {
+            return inventoryInitialOrder.sort();
+            }}        
+
+        const inventoryInitialOrder = await app.inventory.inventoryItemName.allTextContents();
+        await app.inventory.sortDropdown.selectOption({ value: azOption.sort});
+        const actualOrder = await app.inventory.inventoryItemName.allTextContents();
+        const expectedOrder = azOption.getExpectedResult(inventoryInitialOrder)
+        expect(actualOrder).toEqual(expectedOrder);
+        
+    });
+
+    test('sorting lo-hi', async (
+        /** @type {{ app: import('../pages/Application').Application }} */{ app },
+    ) => {
+        await app.login.navigate();
+        await app.login.performLogin('standard_user', 'secret_sauce');
+
+        const lohiOption = {sort: "lohi", getExpectedResult: (inventoryInitialOrder) => {
+             return inventoryInitialOrder.sort((a, b) => Number(a) - Number(b));
+        }}
+        
+        const inventoryInitialOrder = await app.inventory.inventoryPrice.allTextContents();
+        await app.inventory.sortDropdown.selectOption({ value: lohiOption.sort});
+        const inventoryInitialOrderToNumbers = inventoryInitialOrder.map((str) => parseFloat(str.replace('$', '')));
+        const actualOrder = ((await app.inventory.inventoryPrice.allTextContents()).map((str) => parseFloat(str.replace('$', ''))));
+        const expectedOrder = lohiOption.getExpectedResult(inventoryInitialOrderToNumbers)
+        expect(actualOrder).toEqual(expectedOrder);
+       
+    });
+    
+    test('sorting hi-lo', async (
+        /** @type {{ app: import('../pages/Application').Application }} */{ app },
+    ) => {
+        await app.login.navigate();
+        await app.login.performLogin('standard_user', 'secret_sauce');
+
+        const hiloOption =  {sort: "hilo", getExpectedResultPrice: (productPriceInitialOrderToNumbers) => {
+            return productPriceInitialOrderToNumbers.sort((a, b) => Number(b) - Number(a));
+         }}
+
+        const inventoryInitialOrder = await app.inventory.inventoryPrice.allTextContents();
+        await app.inventory.sortDropdown.selectOption({ value: hiloOption.sort});
+        const inventoryInitialOrderToNumbers = inventoryInitialOrder.map((str) => parseFloat(str.replace('$', '')));
+        const actualOrder = ((await app.inventory.inventoryPrice.allTextContents()).map((str) => parseFloat(str.replace('$', ''))));
+        const expectedOrder = hiloOption.getExpectedResultPrice(inventoryInitialOrderToNumbers);
+        expect(actualOrder).toEqual(expectedOrder);
+        
+    });
+});
+
+
+
